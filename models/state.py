aba158a6cbb173a666fe33a4cd83c5b89853f3e7
Madu Jang <madu.jang93@gmail.com>
2024-02-20 16:19:31 -0800

    fix

--- models/state.py
+++ models/state.py
@@ -11,13 +11,10 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        name = ""
-
         @property
         def cities(self):
         """get list of all related city objects"""
